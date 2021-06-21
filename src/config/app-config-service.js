f12758be15d768052146a780bf21f41710d5d368
Poline <krasnova.poline@gmail.com>
2021-06-21 18:19:55 +0300

    update getApiBasePath

--- src/config/app-config-service.js
+++ src/config/app-config-service.js
@@ -2,7 +2,6 @@ import {config} from "./app-config";
 
 export default {
     getApiBasePath() {
-        return 'https://learnsql.ru'
         return `${config.apiSchema}://${config.apiHost}${config.apiPort ? ":" + config.apiPort : ""}`;
     },
 };
